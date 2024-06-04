36235c3b0fd16bbcbb062268a4db57e84cc4117c
luda <3135287831@qq.com>
2024-06-05 05:18:05 +0800

    init

--- Yaocai/views/YaocaiViewSet.py
+++ Yaocai/views/YaocaiViewSet.py
@@ -10,3 +10,14 @@ class YaocaiViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     queryset = Yaocai.objects.all()
     serializer_class = YaocaiSerializer
     filter_backends = [DjangoFilterBackend]
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Yaocai.objects.all()
+        categoryType = self.request.query_params.get('categoryType')
+        if categoryType is not None:
+            queryset = queryset.filter(categoryType=categoryType)
+        return queryset
