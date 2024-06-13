2544f00fd1d8c2dd47c83ecad51f715959161899
luda <3135287831@qq.com>
2024-06-13 22:48:32 +0800

    init

--- Users/views.py
+++ Users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from Users.models import User
+from Users.serializers import UserSerializer
 
 
 @api_view(['POST'])
@@ -8,7 +9,9 @@ def login(request):
     username = request.data.get('username')
     password = request.data.get('password')
     if User.objects.filter(username=username, password=password).exists():
-        return Response({'code': 0, 'message': '登陆成功', 'data': {}})
+        user = User.objects.get(username=username, password=password)
+        serializer = UserSerializer(user)
+        return Response({'code': 0, 'message': '登陆成功', 'data': {serializer.data}})
 
 
 @api_view(['POST'])
@@ -18,5 +21,6 @@ def register(request):
     if User.objects.filter(username=username).exists():
         return Response({'code': 1, 'message': '用户名已存在', 'data': {}})
     else:
-        User.objects.create(username=username, password=password)
-        return Response({'code': 0, 'message': '注册成功', 'data': {}})
+        user = User.objects.create(username=username, password=password)
+        serializer = UserSerializer(user)
+        return Response({'code': 0, 'message': '注册成功', 'data': {serializer.data}})
